b9f5415ea00af1f5144c885cf6e86fefadcfc99d
javohir85 <javohir.85@mail.ru>
2026-08-27 00:48:00 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import ccxt
-
 binance = ccxt.binance({
-    'apiKey': "TNUL5yKBySpLH8sMZ2909iJCqFOCPMtglixlYDA3UoSDwTHIUmtldJEctfhhZnrV"    'secret': 'YOUR_SECRET_KEY',"E1LJpcX6BQwV4ugVjZiSTbIFBYy8l6K6H1pcSt8wv1UaPQvmyHbieisKmEjBjejy"
-
-
+'apiKey': "TNUL5yKBySpLH8sMZ2909iJCqFOCPMtglixlYDA3UoSDwTHIUmtldJEctfhhZnrV"    'secret': "E1LJpcX6BQwV4ugVjZiSTbIFBYy8l6K6H1pcSt8wv1UaPQvmyHbieisKmEjBjejy"
 balance = binance.fetch_balance()
 print(balance)
